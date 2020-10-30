f5f0459d3d2182a6290ce2ac09d90760a1cfd80c
smitptl <smitvptl@gmail.com>
2020-10-30 02:22:38 -0700

    create a home page for amazon-clone and add react-routing to the app

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     //BEM
-    <div className="app">
-      {/* Headr */}
-      <Header />
-      <Home />
-      {/* Home */}
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>YO... This is a checkout Page </h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
